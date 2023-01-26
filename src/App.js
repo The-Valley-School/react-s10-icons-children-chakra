69518f0a85bb698023a5a5b2ed510fb195665211
Fran Linde Blazquez <franlindebl@gmail.com>
2023-01-26 19:43:33 +0100

    Added react-icons library and samples

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { IoAirplane, IoAmericanFootballOutline, IoAccessibilityOutline } from "react-icons/io5";
+import { FcAssistant, FcLinux } from "react-icons/fc";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      
+      <h2>Iconos de Ionicons 5</h2>
+      <IoAirplane className='my-plane' />
+      <IoAmericanFootballOutline className='my-ball' />
+      <IoAccessibilityOutline color="pink" />
+
+      <h2>Iconos flat:</h2>
+      <FcAssistant size={20}></FcAssistant>
+      <FcLinux size={20}></FcLinux>
     </div>
   );
 }
