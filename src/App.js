1db238518b7d0382bf88ec84ea376479075a1119
Fran Linde Blazquez <franlindebl@gmail.com>
2023-01-28 21:18:15 +0100

    Show React.children.count with Collapsable component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,27 @@ import { IoAirplane, IoAmericanFootballOutline, IoAccessibilityOutline } from "r
 import { FcAssistant, FcLinux } from "react-icons/fc";
 import { FaHandPaper, FaHandshakeAltSlash } from "react-icons/fa";
 import CustomButton from "./components/CustomButton/CustomButton";
+import Collapsable from './components/Collapsable/Collapsable';
 
 function App() {
   return (
     <div className="app">
+
+      <h2>Ejemplo de collapsable</h2>
+      <Collapsable title="Este es el primer collapsable">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <p>Integer velit augue, volutpat at libero sit amet, ornare lacinia mi.</p>
+        <p>Nunc hendrerit orci eget interdum aliquam. Mauris vitae justo sodales, pharetra neque sit amet, lacinia tortor.</p>
+      </Collapsable>
+
+      <Collapsable title="Este es otro collapsable">
+        <p>Donec vel nunc augue. Nam auctor metus et lorem interdum aliquet.</p>
+        <ul>
+          <li>Primer elemento</li>
+          <li>Segundo elemento</li>
+          <li>Tercer elemento</li>
+        </ul>
+      </Collapsable>
 
       <h2>Ejemplo de botón custom:</h2>
       <CustomButton action={() => alert("Hola")} size="BIG" color="red">
