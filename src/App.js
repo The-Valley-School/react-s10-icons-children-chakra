a6c1c9d6d8e9d6a8c5b2685fbbd67e52cd65e006
Fran Linde Blazquez <franlindebl@gmail.com>
2023-01-28 12:56:46 +0100

    Added children sample

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
 import { IoAirplane, IoAmericanFootballOutline, IoAccessibilityOutline } from "react-icons/io5";
 import { FcAssistant, FcLinux } from "react-icons/fc";
+import { FaHandPaper, FaHandshakeAltSlash } from "react-icons/fa";
+import CustomButton from "./components/CustomButton/CustomButton";
 
 function App() {
   return (
     <div className="app">
-      
+
+      <h2>Ejemplo de botón custom:</h2>
+      <CustomButton action={() => alert("Hola")} size="BIG" color="red">
+        <span>Eeeeeey!!!</span> <FaHandPaper></FaHandPaper>
+      </CustomButton>
+
+      <CustomButton action={() => alert("Adiós")} color="blue">
+        <span>Adiós!!!</span> <FaHandshakeAltSlash></FaHandshakeAltSlash>
+        <ul>
+          <li>Uno</li>
+          <li>Dos</li>
+        </ul>
+      </CustomButton>
+
       <h2>Iconos de Ionicons 5</h2>
       <IoAirplane className='my-plane' />
       <IoAmericanFootballOutline className='my-ball' />
